7284912ab9bbd2125fc52b9fdc99dc63fe753367
Ana Coronel <a9nna.coronel@gmail.com>
2023-01-31 00:23:50 +0100

    Add the component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
-function App() {
-  return <div className="container"></div>;
-}
+const App = (): JSX.Element => {
+  const gentlemen = [
+    {
+      id: 1,
+      name: "Bertin Osborne",
+      status: "Alive",
+      profession: "Youtuber",
+      twitter: "@osbourne",
+      picture: "img/bertin.jpg",
+      alternativeText: "Osbourne pointing at you",
+      selected: true,
+    },
+    {
+      id: 2,
+      name: "The Farytale",
+      status: "RIP",
+      profession: "Influencer",
+      twitter: "pending",
+      picture: "img/fary.jpg",
+      alternativeText: "The Fary pointing at you",
+      selected: false,
+    },
+    {
+      id: 3,
+      name: "Julius Churchs",
+      status: "Alive",
+      profession: "Java developer",
+      twitter: "@julius_churchs",
+      picture: "img/julio.jpg",
+      alternativeText: "Churchs pointing at you",
+      selected: true,
+    },
+  ];
+
+  return (
+    <div className="container">
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
+    </div>
+  );
+};
 
 export default App;
