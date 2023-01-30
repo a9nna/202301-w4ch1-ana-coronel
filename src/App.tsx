4909d01994415f81f129d5c21727cb91996ec83d
Ana Coronel <a9nna.coronel@gmail.com>
2023-01-31 00:23:50 +0100

    Add the component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div className="container"></div>;
-}
+const App = (): JSX.Element => {
+  return (
+    <div className="container">
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
+    </div>
+  );
+};
 
 export default App;
